97992c1b60f7843d836d987d6b030e5c0741f5e9
Steffen Tröster <steffen.troester@googlemail.com>
2014-08-20 19:02:17 +0200

    adding every underground line and adding some d3 magic

--- src/undergroundjs.js
+++ src/undergroundjs.js
@@ -1,6 +1,10 @@
 (function() {
+
+    var color = d3.scale.category20b();
+
     var height = 600;
     var width = 1140;
+
     var margin = {
         top: 50,
         right: 70,
@@ -16,33 +20,48 @@
     var svg = d3.select("#map")
         .attr("width", width)
         .attr("height", height)
+        .call(d3.behavior.zoom().on("zoom", function() {
+            svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")");
+        }))
         .append("g")
         .attr("transform", "translate(40,0)");
+
+
     d3.json("stations.json", function(error, root) {
 
         if (error) {
             console.log("ERROR!");
             return 0;
         }
+
+
         var node_data_object = {};
         var link_data = [];
+
         root.lines.forEach(function(line) {
             for (var i = 0; i < line.stations.length; i++) {
                 node_data_object[line.stations[i].name] = line.stations[i];
-                if (line.stations[i + 1] != undefined) {
-                    link_data.push({
-                        target: line.stations[i],
-                        source: line.stations[i + 1]
-                    });
-                }
             }
         });
+
+        root.lines.forEach(function(line) {
+            for (var i = 0; i < line.stations.length; i++) {
+                if (line.stations[i + 1] != undefined) {
+                    link_data.push({
+                        target: node_data_object[line.stations[i].name],
+                        source: node_data_object[line.stations[i + 1].name],
+                        line: line.name
+                    });
+                }
+
+            }
+        });
+
         console.log(link_data);
 
         var node_data = Object.keys(node_data_object).map(function(k) {
             return node_data_object[k]
         });
-
 
         function tick() {
             path.attr("d", function(d) {
@@ -65,28 +84,31 @@
             .links(link_data)
             .start();
 
+        var path = svg.append("svg:g").selectAll("path")
+            .data(force.links())
+            .enter().append("svg:path")
+            .attr("class", "link")
+            .attr("stroke", function(d, i) {
+                return color(d.line);
+            })
+            .attr("marker-end", "url(#end)");
+
         var node = svg.selectAll(".node")
             .data(force.nodes())
             .enter().append("g")
             .attr("class", "node")
             .call(force.drag);
 
-        var path = svg.append("svg:g").selectAll("path")
-            .data(force.links())
-            .enter().append("svg:path")
-            .attr("class", "link")
-            .attr("marker-end", "url(#end)");
-
-
         node.append("circle")
-            .attr("r", 5);
+            .attr("r", function(d, i) {
+                return d.weight;
+            });
+
         node.append("text")
             .attr("x", 12)
             .attr("dy", ".35em")
             .text(function(d) {
                 return d.name;
             });
-
     });
-
 })();
